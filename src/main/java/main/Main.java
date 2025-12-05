5b05cfb9280c043ed4908aa371363c8962542b27
CZG-KoR <>
2025-12-05 09:14:47 +0100

    generate structure

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,5 +8,13 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+        
+        
+        // hier werden ausgeteilt
+        
+        
+        
+        
+        // hier werden zwilling verglichen...
     }
 }
