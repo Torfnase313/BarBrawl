41a87897b545c42b094a4b12a5a72b64cd22db21
Katharina <katharina.ensslen@outlook.de>
2026-01-05 10:41:50 +0100

    NPC Klasse ausarbeiten und Eigenschaften und Methoden zuweisen
    Refactor NPC class to use arrays for text and greetings, and add healthpoints and inventory.

--- src/main/java/assets/actors/NPC.java
+++ src/main/java/assets/actors/NPC.java
@@ -3,21 +3,40 @@ package assets.actors;
 
 import assets.GameAsset;
 import java.awt.Image;
+import java.util.Random;
 
 public class NPC extends GameAsset{
     
-    String text;
+    String[] text;
+    int healthpoints;
+    String[] inventar;
+    boolean alive;
+    String[] greetings;
+    String greeting;
     
-    public NPC(int x, int y, Image img, String name) {
-        super(x, y, img, name);
-        this.text = "Hello!";
+    //npc der random begrüßung/beleidigung sagt, der aber getötet werden kann und evtl was dropt
+    public NPC(int x, int y, Image img, String name, int healthpoints, String[] inventar, String[] greetings) {
+        super(x, y, img, name)
+        Random r = new Random();
+        int whichGreeting = r.nextInt(10) //anzahl der unterschiedlichen greetings die es so gibt
+        this.greetings = greetings[whichGreeting];
+        this.alive = true;
+        this.healthpoints = healthpoints;
+        this.inventar = inventar;
     }
-    
-    public NPC(int x, int y, Image img, String name, String text) {
+
+    //npc mit text
+    public NPC(int x, int y, Image img, String name, String[] text, int healthpoints, String[] inventar) {
         super(x, y, img, name);
         this.text = text;
+        this.alive = true;
+        this.healthpoints = healthpoints;
+        this.inventar = inventar;
     }
-        
+
+    public void move() {
+    }
+    
     public String talk() {
         return this.text;
     }
