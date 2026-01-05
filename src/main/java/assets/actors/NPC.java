116856f0f8623dff30d42b57f20389d99db84488
Katharina <katharina.ensslen@outlook.de>
2026-01-05 11:17:49 +0100

    talk-methode wird für beide npc arten als konsolenausgabe gemacht

--- src/main/java/assets/actors/NPC.java
+++ src/main/java/assets/actors/NPC.java
@@ -4,6 +4,7 @@ package assets.actors;
 import assets.GameAsset;
 import java.awt.Image;
 import java.util.Random;
+import java.util.Scanner;
 
 public class NPC extends GameAsset{
     
@@ -13,32 +14,42 @@ public class NPC extends GameAsset{
     boolean alive;
     String[] greetings;
     String greeting;
+    boolean quest;
     
     //npc der random begrüßung/beleidigung sagt, der aber getötet werden kann und evtl was dropt
-    public NPC(int x, int y, Image img, String name, int healthpoints, String[] inventar, String[] greetings) {
+    public NPC(int x, int y, Image img, String name, int healthpoints, String[] inventar) {
         super(x, y, img, name)
         Random r = new Random();
-        int whichGreeting = r.nextInt(10) //anzahl der unterschiedlichen greetings die es so gibt
-        this.greetings = greetings[whichGreeting];
+        this.greetings = r.nextInt(greetings.length()); //unterschiedliche, random greetings werden generiert
         this.alive = true;
         this.healthpoints = healthpoints;
         this.inventar = inventar;
+        this.quest = false;
     }
 
-    //npc mit text
+    //npc mit text, der zu questline gehört, kann noch getötet werden --> evtl ändern
     public NPC(int x, int y, Image img, String name, String[] text, int healthpoints, String[] inventar) {
         super(x, y, img, name);
         this.text = text;
         this.alive = true;
         this.healthpoints = healthpoints;
         this.inventar = inventar;
+        this.quest = true;
     }
 
     public void move() {
     }
     
     public String talk() {
-        return this.text;
+        if this.quest = false
+            {System.out.println(greeting);
+            }else{
+            for(int i = 0; i < text.length(); i++)
+                {System.put.println(text[i]);
+                 Scanner s = new Scanner(System.in);
+                 s.next(); //wenn enter gedrückt wird, wird nächster text ausgegeben
+                }
+        }
     }
  
 }
